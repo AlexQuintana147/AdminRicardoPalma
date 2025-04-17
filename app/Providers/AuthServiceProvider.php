ffa8ef57e8a302c36bf8f9988f990075b5f6cb81
AlexQuintana147 <alexquintana12622@hotmail.com>
2025-04-17 16:47:39 -0500

    fixerrores

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,8 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-
-class AuthServiceProvider extends ServiceProvider
+use App\Models\Cita;
 {
     /**
      * The policy mappings for the application.
